061ac74403b3ed32b522fe482e2cc0a165f2d5e5
youssefmsaadi <youssefmsaadi95@gmail.com>
2022-07-07 10:50:48 +0100

    added class-validator and class-transformer and setup the user creaate dto

--- src/models/user/dto/create-user.dto.ts
+++ src/models/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    username: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    passport: string;
+
+    @IsOptional()
+    @IsString()
+    firstName: string;
+
+    @IsOptional()
+    @IsString()
+    lastName: string;
+}
