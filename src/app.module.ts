2346b00589b93052ef136032bb8422896f3f2190
youssefmsaadi <youssefmsaadi95@gmail.com>
2022-07-25 11:55:55 +0100

    added game resource/endpoints (empty for now)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './models/user/user.module';
 import { AuthModule } from './models/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { GameModule } from './game/game.module';
 
 @Module({
     imports: [UserModule, AuthModule, ConfigModule.forRoot({
         isGlobal: true,
-    })],
+    }), GameModule],
 })
 export class AppModule {
 }
